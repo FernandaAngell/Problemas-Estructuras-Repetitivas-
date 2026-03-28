ee971b19ab3a1cd650c5527de35448d848bd0cd6
Maria Fernanda Tolosa <fernandaangel242@gmail.com>
2026-03-28 16:52:42 -0500

    Update control_vecinos.py

--- control_vecinos.py
+++ control_vecinos.py
@@ -3,6 +3,20 @@
 #FUNDAMENTOS DE PROGRAMACION
 #CODIGO FUENTE: AUTORIA PROPIA
 
+#En un conjunto residencial, cada vecino debe pagar una
+#cuota mensual para el mantenimiento. El administrador quiere un
+#programa que revise uno por uno a todos los vecinos para lo cual el
+#usuario debe ingresar la cantidad de residentes de la unidad residencial
+#y el valor de la cuota mensual.
+#Por cada vecino, el sistema debe pedir si ya pagó su cuota o no.
+#Si el vecino pagó, se registra como “al día”; si no pagó, el programa lo
+#registra como “moroso”.
+#Este proceso debe repetirse hasta haber revisado a todos los vecinos.
+#Al finalizar, el programa muestra cuántas personas están al día y
+#cuántas están atrasadas y cuanto es el valor total adeudado por los
+#vecinos morosos.
+
+
 # Solicitar datos iniciales
 total_vecinos = int(input("Ingrese la cantidad de residentes: "))
 cuota = float(input("Ingrese el valor de la cuota mensual: "))
@@ -28,3 +42,5 @@ print("\n=== REPORTE FINAL ===")
 print(f"Vecinos al día: {al_dia}")
 print(f"Vecinos morosos: {morosos}")
 print(f"Total adeudado: ${total_deuda}")
+
+#Realizado por Maria Fernanda Tolosa Angel
